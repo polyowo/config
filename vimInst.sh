be203fab9b11d4045df4aea36c8187e8c65c92d3
polyowo <polygon759@gmail.com>
2023-11-27 14:40:44 +0700

    update vim instsh

--- vimInst.sh
+++ vimInst.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 mv .vimrc ~/.vimrc
+
+#install you complete me
+git clone --recurse-submodules https://github.com/ycm-core/YouCompleteMe.git ~/.vim/pack/YouCompleteMe/opt/YouCompleteMe
+pushd ~/.vim/pack/YouCompleteMe/opt/YouCompleteMe
+./install.py --all
+popd
+
+#install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+#install vimplug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
 vim +PluginInstall +PlugInstall
 
