e89eead98bb959566e58097c5ce2fcc431666139
polyowo <polygon759@gmail.com>
2023-11-27 14:32:47 +0700

    update vim instsh

--- vimInst.sh
+++ vimInst.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mv .vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
